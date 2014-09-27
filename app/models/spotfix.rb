b349dc1a82f7a56a6edcde9fe6c82f5c0c1eafd8
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2014-09-27 17:42:48 -0400

    Add validation to the spotfix model

--- app/models/spotfix.rb
+++ app/models/spotfix.rb
@@ -1,4 +1,14 @@
 class Spotfix < ActiveRecord::Base
+
+  module Status
+    PENDING = 'pending'
+    FIXED = 'fixed'
+  end
+
   belongs_to :owner, class_name: 'User'
   has_many :replies
+
+  validates_presence_of :title, :status, :latitude, :longitude, :fix_date
+  validates_inclusion_of :status, in: [Status::PENDING, Status::FIXED]
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,14 @@
 class User < ActiveRecord::Base
-  validates_presence_of :first_name, :last_name
-  validates :email, presence: true, uniqueness: { case_sensitive: false }
-  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
 
   before_save { self.email = email.downcase }
-  has_secure_password
-  validates :password, length: { minimum: 6 }
 
   has_many :spotfixes, foreign_key: :owner_id
   has_many :replies
+
+  validates_presence_of :first_name, :last_name
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+
 end
