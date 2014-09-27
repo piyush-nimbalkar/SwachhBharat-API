009372d205713d3c9e49c0038b395bda15a49cd1
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2014-09-27 00:32:34 -0400

    Adding an index and making emails unique. Also storing them in downcase

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   validates_presence_of :first_name, :last_name
-  validates :email, presence: true, uniqueness: {case_sensitive: false}
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
+
+  before_save { self.email = email.downcase }
 end
